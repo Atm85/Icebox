e2b00e39692d5bb1e8779c1edda180609c5f90cc
steve <stevecoulter99@gmail.com>
2020-08-13 20:21:19 -0500

    initial commit

---
+++ src/main/java/Icebox/Main.java
@@ -0,0 +1,12 @@
+package Icebox;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class Main extends PluginBase {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Break the ice with Icebox, chat formatter by Atom#7489");
+    }
+}
+
