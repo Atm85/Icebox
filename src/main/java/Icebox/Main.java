114b3ab2294127337cc983cf0c10b8d349e3ba16
iAtomPlaza <stevecoulter99@gmail.com>
2020-08-13 20:56:37 -0500

    load configuration on startup

--- src/main/java/Icebox/Main.java
+++ src/main/java/Icebox/Main.java
@@ -2,11 +2,20 @@ package Icebox;
 
 import Icebox.events.*;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
 
 public class Main extends PluginBase {
 
+    public Config config;
+
     @Override
     public void onEnable() {
+
+        // load configuration
+        saveDefaultConfig();
+        config = getConfig();
+
+        // register plugin events
         getServer().getPluginManager().registerEvents(new JoinEvent(this), this);
         getServer().getPluginManager().registerEvents(new QuitEvent(this), this);
         getServer().getPluginManager().registerEvents(new ChatEvent(this), this);
