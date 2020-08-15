364c6390bc7830ad72d17d08d519d4040e35c608
iAtomPlaza <stevecoulter99@gmail.com>
2020-08-14 20:59:06 -0500

    save all groups from luckperms into config

--- src/main/java/Icebox/Main.java
+++ src/main/java/Icebox/Main.java
@@ -1,9 +1,16 @@
 package Icebox;
 
 import Icebox.events.*;
+import cn.nukkit.Player;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
 import com.creeperface.nukkit.placeholderapi.api.PlaceholderAPI;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.group.Group;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main extends PluginBase {
 
@@ -13,9 +20,21 @@ public class Main extends PluginBase {
     public Config config;
 
     /**
+     * Map containing the formatted string for a players chat output
+     */
+    public Map<String, String> chatFormat;
+
+    /**
+     * Map containing the formatted string for a players nametag
+     */
+    public Map<String, String> nametagFormat;
+
+    /**
      * PlaceholderAPI
      */
     public PlaceholderAPI placeholderAPI;
+
+    public LuckPerms luckPerms;
 
     @Override
     public void onEnable() {
@@ -27,10 +46,41 @@ public class Main extends PluginBase {
         // load placeholder api
         placeholderAPI = PlaceholderAPI.getInstance();
 
+        // load luckperms api
+        luckPerms = LuckPermsProvider.get();
+
         // register plugin events
         getServer().getPluginManager().registerEvents(new JoinEvent(this), this);
         getServer().getPluginManager().registerEvents(new QuitEvent(this), this);
         getServer().getPluginManager().registerEvents(new ChatEvent(this), this);
+
+        // create default formats for akk groups provided from luckperms
+        loadAllGroups();
+    }
+
+    public void loadAllGroups() {
+        for (Group group : luckPerms.getGroupManager().getLoadedGroups()) {
+            if (getGroupFormats(group.getName()) == null) {
+                Map<String, String> formatData = new HashMap<>();
+                formatData.put("chat-format", "&l&8"+group.getName()+" &r%player% > %msg%");
+                formatData.put("nametag", "&l&8"+group.getName()+" &r%player%");
+                config.set(group.getName(), formatData);
+            }
+        }
+
+        config.save();
+    }
+
+    public Map<String, String> getGroupFormats(String group) {
+        return (Map<String, String>) config.get(group);
+    }
+
+    public String getChatFormat(Player player) {
+        return placeholderAPI.translateString(chatFormat.get(player.getName()), player);
+    }
+
+    public String getNametagFormat(Player player) {
+        return placeholderAPI.translateString(nametagFormat.get(player.getName()), player);
     }
 }
 
