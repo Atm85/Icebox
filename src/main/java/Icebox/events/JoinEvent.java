0ca3bb95d1d933dfe1a6ee489a483ba7e81384ac
iAtomPlaza <stevecoulter99@gmail.com>
2020-08-14 20:59:52 -0500

    add player into data to use later

--- src/main/java/Icebox/events/JoinEvent.java
+++ src/main/java/Icebox/events/JoinEvent.java
@@ -1,10 +1,14 @@
 package Icebox.events;
 
 import Icebox.Main;
+import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
+import net.luckperms.api.model.user.User;
+
+import java.util.Map;
 
 public class JoinEvent implements Listener {
 
@@ -17,5 +21,20 @@ public class JoinEvent implements Listener {
     @EventHandler(priority = EventPriority.NORMAL)
     public void onJoin(PlayerJoinEvent event) {
 
+        Player player = event.getPlayer();
+        User luckpermsUser = plugin.luckPerms.getUserManager().getUser(player.getUniqueId());
+
+        assert luckpermsUser != null;
+        String group = luckpermsUser.getPrimaryGroup();
+
+        Map<String, String> configValue = (Map<String, String>) plugin.config.get(group);
+        if (configValue == null) {
+            luckpermsUser.setPrimaryGroup("default");
+            configValue = (Map<String, String>) plugin.config.get("default");
+            plugin.getLogger().warning("group " + group + " format not fount! setting default group format");
+        }
+
+        plugin.chatFormat.put(player.getName(), configValue.get("chat-format"));
+        plugin.nametagFormat.put(player.getName(), configValue.get("nametag"));
     }
 }
