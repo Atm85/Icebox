87f23115bcbcb693c62f795aa9eaa938abd1ef94
iAtomPlaza <stevecoulter99@gmail.com>
2020-08-14 21:50:18 -0500

    is this plugin done now?

--- src/main/java/Icebox/events/ChatEvent.java
+++ src/main/java/Icebox/events/ChatEvent.java
@@ -1,10 +1,12 @@
 package Icebox.events;
 
 import Icebox.Main;
+import cn.nukkit.Player;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerChatEvent;
+import cn.nukkit.utils.TextFormat;
 
 public class ChatEvent implements Listener {
 
@@ -16,6 +18,10 @@ public class ChatEvent implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onChat(PlayerChatEvent event) {
-
+        Player player = event.getPlayer();
+        String content = event.getMessage();
+        String raw_content = plugin.getChatFormat(player);
+        String format_content = raw_content.replaceAll("msg", content);
+        event.setFormat(TextFormat.colorize(format_content));
     }
 }
